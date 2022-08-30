637eff107b91593a8b2e91f71ce332450c6406c3
Phillip Kelly <phillipbentonkelly@gmail.com>
2022-08-30 14:20:29 -0400

    DIG-388: Separating Permit Finder web app from Digital Repo - Init Repo with only Permit-Finder

---
+++ modules-js/config-babel/typescript.js
@@ -0,0 +1,15 @@
+/**
+ * Babel preset that includes `@babel/preset-typescript` to handle stripping
+ * TypeScript’s syntax. Also includes the transforms necessary for
+ * TypeScript-compatible decorators and static properties.
+ *
+ * Every .babelrc in this repo should probably include this preset.
+ */
+module.exports = () => ({
+  presets: [require('@babel/preset-typescript')],
+  plugins: [
+    [require('@babel/plugin-proposal-decorators'), { legacy: true }],
+    [require('@babel/plugin-proposal-class-properties'), { loose: true }],
+    [require('@babel/plugin-proposal-private-methods'), { loose: true }],
+  ],
+});
